73bda98d56fb549a5612c44a38f3bf5d843f5b33
Brisheyka Lopez <brisheyka.lopez@ucb.edu.bo>
2026-03-29 18:58:05 -0400

    feat: configuración de Spring Boot, conexión a Supabase y creación de RestauranteController

---
+++ maps-backend/src/test/java/com/antojito/maps_backend/MapsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antojito.maps_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
